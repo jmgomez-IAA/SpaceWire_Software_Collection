f0b513a2c53e61edabd361229f95dda0b318a0d6
Juan Manuel Gómez <jmgomez@iaa.es>
2019-12-12 10:48:17 +0100

    Add application to  a test to detect the avilable STAR Dundee devices

---
+++ src/test_list_devices.c
@@ -0,0 +1,217 @@
+/*
+  @file test_list_devices.c
+  @author Juan Manuel Gómez
+  @brief Check for SpaceWire devices connected and list it ids.
+  @details 
+  @remark 
+  @todo 
+  @param No parammeters needed.
+  @example 
+  @copyright jmgomez CSIC-IAA
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utility.h"
+#include "star-dundee_types.h"
+#include "star-api.h"
+#include "cfg_api_router.h"
+#include "cfg_api_mk2.h"
+#include "cfg_api_mk2_types.h"
+#include "cfg_api_brick_mk3.h"
+
+#define VERSION_INFO "star-system_test v2.0"
+
+#include <errno.h>
+
+#define STAR_INFINITE 30000
+#define _TX_INTERFACE 1
+#define _RX_INTERFACE 2
+#define _PACKET_SIZE 64
+
+#define _TX_BAUDRATE_MUL 2
+#define _TX_BAUDRATE_DIV 4
+#define _RX_BAUDRATE_MUL 2
+#define _RX_BAUDRATE_DIV 4
+
+#define _ADDRESS_PATH 0xFE
+#define _ADDRESS_PATH_SIZE 1
+
+
+/* 
+  Flags and variables requiered to manage
+  the input parammeteres 
+*/
+static int verbose_flag;
+static int reset_flag;
+static int identify_flag;
+static unsigned int verbose_level;
+
+int parse_parammeters (int paramc, char *const *paramv)
+{
+  int opt;
+
+  if (paramc == 1)
+  {
+    fprintf (stderr,"Argument expected.\n");
+    fprintf (stderr,"Sintax: %s\t -v -r -i\n", paramv[0]);
+    fprintf (stderr,"Check for SpaceWire devices connected.\n");
+    fprintf (stderr,"\t-v\t\t verbose mode.");
+    fprintf (stderr,"\t-r reset the devices.\n");
+    fprintf (stderr,"\t-i identify the devices.\n");
+    fprintf (stderr,"\n\n");
+    return -1;
+  }
+
+  while ((opt = getopt (paramc, paramv, "vri")) != -1)
+  {
+    switch (opt)
+    {
+      case 'v':  //verbose mode
+        verbose_flag = 1;
+        verbose_level = 1;
+        if (verbose_flag == 1) fprintf (stderr,"Verbose mode enabled\n");
+        break;
+
+      case 'i':  //Identify flags
+        identify_flag = 1;        
+        if (verbose_flag == 1) fprintf (stderr,"Indentify device enabled\n");
+        break;
+
+      case 'r':  //reset device
+        reset_flag = 1;        
+        if (verbose_flag == 1)  fprintf (stderr,"Reset device enabled\n");
+        break;
+
+      default:
+        fprintf (stderr, "Sintax error argument expected.\n");
+        return 1;
+    }
+  }
+
+  for(; optind < paramc; optind++)
+  {
+    fprintf(stderr, "Extra arguments detected: %s\n", paramv[optind]);
+    return optind;
+  } 
+
+  return opt;
+}
+
+
+/******************************************************************/
+/*                                                                */
+/*****************************************************************/
+int __cdecl main(int argc, char *argv[]){
+
+  STAR_DEVICE_ID* devices;
+  STAR_DEVICE_ID deviceId;
+  U32 deviceCount = 0U, deviceActual = 0U;
+
+  STAR_CHANNEL_MASK channelMask;
+  STAR_CFG_MK2_HARDWARE_INFO hardwareInfo;
+  char versionStr[STAR_CFG_MK2_VERSION_STR_MAX_LEN];
+  char buildDateStr[STAR_CFG_MK2_BUILD_DATE_STR_MAX_LEN];
+  char device_name_list[10][64];
+  char device_serial_list[10][64];
+  char *device_name;
+  char *device_serial;
+
+  verbose_flag    = 0;  
+  identify_flag   = 0;
+  reset_flag      = 0;
+
+  verbose_level   = 0;
+  
+
+    // Parse parammeteres
+  if ( parse_parammeters (argc, argv) != -1){
+    fprintf (stderr, "There are unkown parammeters,please check!!\n");
+    return -1;
+  }
+
+
+  /***************************************************************/
+  /*        Detect SpaceWire devices                             */
+  /*                                                             */
+  /***************************************************************/
+  
+  /* Get the list of devices of the specified type */
+  devices = STAR_getDeviceListForType(STAR_DEVICE_TXRX_SUPPORTED , &deviceCount);
+  if (devices == NULL){
+    fprintf (stderr, "No device found\n");	
+    return 0;
+  } 
+
+  for (deviceActual = 0; deviceActual < deviceCount; deviceActual ++)
+  {
+    /* We are working with the first device, what if I have more? */
+    deviceId = devices[deviceActual];
+    if (deviceId == STAR_DEVICE_UNKNOWN)
+    {
+      fprintf (stderr, "Error dispositivo desconocido.\n");    
+    }
+    
+    device_name = STAR_getDeviceName(deviceId);
+    if (device_name == NULL)
+    {
+      fprintf (stderr, "Device name error.\n");
+    }
+
+    /* Get hardware info*/
+    CFG_MK2_getHardwareInfo(deviceId, &hardwareInfo);
+    CFG_MK2_hardwareInfoToString(hardwareInfo, versionStr, buildDateStr);
+
+    /* Get device Serial Number */
+    device_serial = STAR_getDeviceSerialNumber(deviceId);    
+
+    /* Display the hardware info*/
+    if (verbose_level == 1)
+    {
+      fprintf (stdout, "Version: %s", versionStr);
+      fprintf (stdout, "BuildDate: %s", buildDateStr);  
+    }
+
+    if (identify_flag == 1)
+    {
+      // Flashes the LEDS.
+      if (CFG_MK2_identify(deviceId) == 0)      
+        fprintf (stderr, "\nERROR: Unable to identify device");        
+      
+
+    }
+
+    if (reset_flag == 1)
+    {
+      if (STAR_resetDevice(deviceId) == 0)
+        fprintf (stderr, "STAR: Reset the device\n");
+    }
+
+
+    strcpy(device_name_list[deviceActual], device_name);
+    strcpy(device_serial_list[deviceActual], device_serial);
+
+    STAR_destroyString(device_name);
+    STAR_destroyString(device_serial);
+  }
+
+  if (deviceCount > 0)
+  {
+    fprintf(stdout, "{\"Status\" : \"Success\", \"DeviceNumber\":%d, \"DeviceList\" : [",  deviceCount);
+      for (deviceActual = 0; deviceActual < deviceCount; deviceActual ++)
+          fprintf(stdout, "{\"DeviceId\":%d,\"DeviceSerial\":\"%s\"}",  deviceActual, device_serial_list[deviceActual]);
+        
+    fprintf(stdout, "]}\n");
+  }
+  else
+  {
+    fprintf(stdout, "{\"Status\" : Fail}\n");
+  }
+
+  return 0;
+}
+
+
+
+
+
--- src/test_receiv.c
+++ src/test_receiv.c
@@ -1,7 +1,7 @@
 /*
   @file test_receiv.c
   @author Juan Manuel Gómez
-  @brief Receive files forever and store it on a file.
+  @brief Receive files forever and dumps to stdio.
   @details 
   @param No parammeters needed.
   @example ./test_rmap  
@@ -40,7 +40,7 @@ clock_t GET_TIME()
 #define STAR_INFINITE 30000
 
 #define _TX_INTERFACE 1
-#define _RX_INTERFACE 2
+#define _RX_INTERFACE 1
 #define _PACKET_SIZE 64
 
 #define _TX_BAUDRATE_MUL 2
@@ -51,6 +51,102 @@ clock_t GET_TIME()
 #define _ADDRESS_PATH 2
 #define _ADDRESS_PATH_SIZE 1
 
+
+/* 
+  Flags and variables requiered to manage
+  the input parammeteres 
+*/
+static int verbose_flag;
+static int reset_flag;
+static int identify_flag;
+static int address_flag;
+static int size_flag;
+static int serial_flag;
+static int timeout_flag;
+
+static unsigned int verbose_level;
+static unsigned int address_to_write;
+static unsigned int packet_data_size;
+static unsigned int op_serial;
+static unsigned int op_timeout;
+
+int parse_parammeters (int paramc, char *const *paramv)
+{
+  int opt;
+
+  if (paramc == 1)
+  {
+    fprintf (stderr,"One argument expected.\n");
+    fprintf (stderr,"Sixtax: %s -v -s serial_number -a targetAddress -b byteSize -p targetPath -t timeout\n", paramv[0]);
+    fprintf (stderr,"Transmit a SpaceWire packet with random data.\n");    
+    fprintf (stderr,"\t-s serial_number\t STAR-DUNDEE device serial number.\n");
+    fprintf (stderr,"\t-t timeout\t milliseconds to wait for the reception of the reply.\n");
+    fprintf (stderr,"\t-v\t\t verbose mode.");
+    fprintf (stderr,"\n\n");
+    return -1;
+  }
+
+  while ((opt = getopt (paramc, paramv, "vris:t:")) != -1)
+  {
+    switch (opt)
+    {
+      case 'v':  //verbose mode
+        verbose_flag = 1;
+        verbose_level = 1;
+        if (verbose_flag == 1) fprintf (stderr,"Verbose mode enabled\n");
+        break;
+
+      case 'i':  //Identify flags
+        identify_flag = 1;        
+        if (verbose_flag == 1) fprintf (stderr,"Indentify device enabled\n");
+        break;
+
+      case 'r':  //reset device
+        reset_flag = 1;        
+        if (verbose_flag == 1)  fprintf (stderr,"Reset device enabled %s\n", optarg);
+        break;
+
+      case 's': //Device Serial number
+        serial_flag = 1;        
+        unsigned long param_serial = strtoul(optarg, NULL, 10);
+        op_serial = (unsigned int) param_serial;
+        if (verbose_flag == 1) fprintf (stderr,"The argument supplied is %u\n", op_serial);
+        break;
+
+      case 't': //Time-out
+        timeout_flag = 1;        
+        unsigned long param_timeout = strtoul(optarg, NULL, 10);
+        op_timeout = (unsigned int) param_timeout;
+        if (verbose_flag == 1) fprintf (stderr,"The argument supplied is %u\n", op_timeout);
+        break;
+
+      case '?':
+        if ( (optopt == 'a') || (optopt == 't') )
+          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+        else if (isprint (optopt))
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        else
+          fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+        return 1;
+
+      default:
+        fprintf (stderr, "One argument expected.\n");
+        return 1;
+    }
+  }
+
+  for(; optind < paramc; optind++)
+  {
+    fprintf(stderr, "extra arguments: %s\n", paramv[optind]);
+    return optind;
+  } 
+
+  return opt;
+}
+
+
 /******************************************************************/
 /*                                                                */
 /*****************************************************************/
@@ -59,16 +155,36 @@ int __cdecl main(int argc, char *argv[]){
   STAR_DEVICE_ID* devices;
   STAR_DEVICE_ID deviceId;
   U32 deviceCount = 0U, i;
+  unsigned int deviceActual = 0U;
+  char *device_serial;  
 
   STAR_CHANNEL_MASK channelMask;
   STAR_CFG_MK2_HARDWARE_INFO hardwareInfo;
   char versionStr[STAR_CFG_MK2_VERSION_STR_MAX_LEN];
   char buildDateStr[STAR_CFG_MK2_BUILD_DATE_STR_MAX_LEN];
 
-  unsigned int txChannelNumber, rxChannelNumber;
-  STAR_CHANNEL_ID rxChannelId = 0U, txChannelId = 0U;
+  unsigned int rxChannelNumber;
+  STAR_CHANNEL_ID rxChannelId = 0U;
   STAR_CFG_MK2_BASE_TRANSMIT_CLOCK clockRateParams;
 
+  verbose_flag    = 0;
+  verbose_level   = 0;
+  identify_flag   = 0;
+  reset_flag      = 0;
+  timeout_flag    = 0;
+  serial_flag     = 0;
+
+      // Parse parammeteres
+  if ( parse_parammeters (argc, argv) != -1){
+    fprintf (stderr, "There are unkown parammeters,please check!!\n");
+    return -1;
+  }
+
+  if (timeout_flag == 0)
+  {
+    fprintf (stderr, "Timeout for the reply reception to maximum.\n");
+    op_timeout = STAR_INFINITE;
+  }
 
   /***************************************************************/
   /*        Configuration                                        */
@@ -79,37 +195,77 @@ int __cdecl main(int argc, char *argv[]){
   /* Packet Size = 1 KB                                          */
   /***************************************************************/
 
-  txChannelNumber = _TX_INTERFACE;
   rxChannelNumber = _RX_INTERFACE;
   clockRateParams.multiplier = _TX_BAUDRATE_MUL;
   clockRateParams.divisor = _TX_BAUDRATE_DIV;
   
-
   /* Get the list of devices of the specified type */
   devices = STAR_getDeviceListForType(STAR_DEVICE_TXRX_SUPPORTED , &deviceCount);
   if (devices == NULL){
-    puts("No device found\n");	
+    fprintf (stderr, "No device found\n");	
     return 0;
   } 
 
-  /* We are working with the first device, what if I have more? */
-  deviceId = devices[0];	
-  if (deviceId == STAR_DEVICE_UNKNOWN){
-    puts("Error dispositivo desconocido.\n");
-    return 0;
+  if (serial_flag == 1)
+  {
+    /* Search the device with the serial number specified. */
+    for (deviceActual = 0; deviceActual < deviceCount; deviceActual ++)
+    {      
+
+      if (devices[deviceActual] == STAR_DEVICE_UNKNOWN){
+        fprintf (stderr, "Error dispositivo desconocido.\n");
+      }
+      else
+      {
+        /* Get device Serial Number */
+        device_serial = STAR_getDeviceSerialNumber(devices[deviceActual]);
+        unsigned long dev_serial = strtoul(device_serial, NULL, 10);
+        if (verbose_level == 1)
+        {
+          fprintf (stderr, "Device %d serial number is %s.\n", deviceActual, device_serial);
+        }
+
+        if (dev_serial == op_serial)
+        {
+          deviceId = devices[deviceActual];
+
+          if (verbose_level == 1)
+            {
+              fprintf (stderr, "Select device %d serial number is %s.\n", deviceActual, device_serial);
+            }
+        }
+
+        STAR_destroyString(device_serial);
+      }
+      
+
+    }
+
+  }
+  else
+  {
+    /* We are working with the first device, what if I have more? */  
+    deviceId = devices[0];
   }
 
   /* Get hardware info*/
   CFG_MK2_getHardwareInfo(deviceId, &hardwareInfo);
   CFG_MK2_hardwareInfoToString(hardwareInfo, versionStr, buildDateStr);
   /* Display the hardware info*/
-  printf("\nVersion: %s", versionStr);
-  printf("\nBuildDate: %s", buildDateStr);	
+  if (verbose_level == 1)
+    {
+      fprintf(stderr, "Version: %s\n", versionStr);
+      fprintf(stderr, "BuildDate: %s\n", buildDateStr);	
+    }
+
   // Flashes the LEDS.
-  if (CFG_MK2_identify(deviceId) == 0)
+  if (identify_flag == 1)
   {
-    puts("\nERROR: Unable to identify device");
-    return 0U;
+    if (CFG_MK2_identify(deviceId) == 0)
+    {
+     fprintf (stderr, "ERROR: Unable to identify device.\n");
+     return 0U;
+    }
   }
 
   /* Get the channels present on the device  */
@@ -117,64 +273,47 @@ int __cdecl main(int argc, char *argv[]){
   channelMask = STAR_getDeviceChannels(deviceId);
   if (channelMask != 7U)
   {
-    puts("The device does not have channel 1 and 2 available.\n");
+    fprintf (stderr, "The device does not have channel 1 and 2 available.\n");
     return 0U;
   }
 
   int status_link = 0;
-  status_link = CFG_BRICK_MK3_setBaseTransmitClock(deviceId, txChannelNumber, clockRateParams);	
+  status_link = CFG_BRICK_MK3_setBaseTransmitClock(deviceId, rxChannelNumber, clockRateParams);	
   if (status_link == 0){
-    puts("Error setting the Tx link speed.\n");	
+    fprintf (stderr, "Error setting the Tx link speed.\n");	
   }
 
-  status_link = CFG_BRICK_MK3_setBaseTransmitClock(deviceId, rxChannelNumber, clockRateParams );
-  if (status_link == 0){
-    puts("Error setting the RX link speed.\n");	
-  }
-
-  /* Open channel 1 for Transmit*/
-  txChannelId = STAR_openChannelToLocalDevice(deviceId, STAR_CHANNEL_DIRECTION_INOUT,
-					      txChannelNumber, TRUE);
-  if (txChannelNumber == 0U)
-  {
-    puts("\nERROR: Unable to open TX channel 1\n");
-    return 0;
-  }
-  /* Open channel 2 for Receipt*/
+  /* Open channel 1 for Receive Operation*/
   rxChannelId = STAR_openChannelToLocalDevice(deviceId, STAR_CHANNEL_DIRECTION_IN,
 					      rxChannelNumber, TRUE);
-  if (rxChannelId == 0U)
+  if (rxChannelNumber == 0U)
   {
-    puts("\nERROR: Unable to open RX channel 2\n");
+    fprintf (stderr, "ERROR: Unable to open channel 1 to receive.\n");
     return 0;
-  }
+  }  
 
-  puts("Channels Opened.\n");	
+  if (verbose_level == 1)
+  {
+    fprintf (stderr, "Channels Opened.\n");  
+  }
+  
 	
   /*****************************************************************/
-  /*    Allocate memory for the transmit buffer and construct      */
-  /*    the packet to transmit.                                    */
+  /*    Create the RX operation                                    */
+  /*                                                               */
   /*****************************************************************/
   unsigned long byteSize = 0;
 
-  void *pFillPacket, *pBuildPacket;
-  unsigned long fillPacketLenCalculated, fillPacketLen;  
-  unsigned long buildPacketLen;
-  U8 pTarget[] = {0,254};
-  U8 pReply[] = {254};
-  U8 pData[] = {0x00, 0x14, 0x02, 0x2E};
-  char status;
-
   byteSize = _PACKET_SIZE;
   STAR_TRANSFER_STATUS rxStatus;
-  STAR_TRANSFER_OPERATION *pTxTransferOp = NULL, *pRxTransferOp = NULL;
+  STAR_TRANSFER_OPERATION *pRxTransferOp = NULL;
   STAR_STREAM_ITEM *pTxStreamItem = NULL;
    
   /* Create the receive operations. */
   pRxTransferOp = STAR_createRxOperation(1, STAR_RECEIVE_PACKETS);
   if (pRxTransferOp == NULL)
   {
-    puts("\nERROR: Unable to create receive operation");
+    fprintf (stderr, "ERROR: Unable to create receive operation\n");
     return 0;
   }
 
@@ -186,19 +325,18 @@ int __cdecl main(int argc, char *argv[]){
   while(counter < 20 || (rxStatus == STAR_TRANSFER_STATUS_COMPLETE))
   {
     /* Submit the receive operation */
-    if (STAR_submitTransferOperation(txChannelId, pRxTransferOp) == 0)
+    if (STAR_submitTransferOperation(rxChannelId, pRxTransferOp) == 0)
     {
-      printf("\nERROR occurred during receive.  Test Tfailed.\n");
+      fprintf (stderr, "ERROR occurred during receive.  Test Tfailed.\n");
       return 0;
     }
 
     /* Wait on the receive operation completing */
     rxStatus = STAR_waitOnTransferOperationCompletion(pRxTransferOp,
-                                                      STAR_INFINITE);
+                                                      op_timeout);
     if (rxStatus != STAR_TRANSFER_STATUS_COMPLETE)
     {
-      printf("\nERROR occurred during receive.  Test failed.\n");
-      return 0;
+      fprintf (stderr, "ERROR occurred during receive.  Test failed.\n");      
     }
 
     // Store the received packet in the file.
@@ -206,7 +344,7 @@ int __cdecl main(int argc, char *argv[]){
     unsigned int rxPacketCount = STAR_getTransferItemCount(pRxTransferOp);
     if (rxPacketCount == 0)
     {
-      printf("No packets received.\n");
+      fprintf (stderr, "No packets received.\n");
     }
     else
     {
@@ -219,7 +357,7 @@ int __cdecl main(int argc, char *argv[]){
         if ((pRxStreamItem == NULL) || (pRxStreamItem->item == NULL) ||
             (pRxStreamItem->itemType != STAR_STREAM_ITEM_TYPE_SPACEWIRE_PACKET) )
         {
-          printf("\nERROR received an unexpected traffic type, or empty traffic item in item %lu\n", i);
+          fprintf (stderr, "ERROR received an unexpected traffic type, or empty traffic item in item %lu\n", i);
         }
         else
         {
@@ -233,14 +371,14 @@ int __cdecl main(int argc, char *argv[]){
 
           printf("\n");
 
-          printf("Packet Number:\t%d\n", counter);
+          fprintf (stderr, "Packet Number:\t%d\n", counter);
           int dat_iter = 0;
           for ( dat_iter= 0; dat_iter < streamDataSize; ++dat_iter){
 
-            printf( "\t0x%x" ,pPacketBufferData[dat_iter]);
+            fprintf (stderr, "\t0x%x" ,pPacketBufferData[dat_iter]);
             if (! ((dat_iter+1) % 8) )
             {
-              printf("\n");
+              fprintf (stderr, "\n");
             }
           }
 
@@ -260,29 +398,16 @@ int __cdecl main(int argc, char *argv[]){
   /*                                                              */
   /****************************************************************/
 
-  /* Dispose of the transfer operations */
-  if (pTxTransferOp != NULL)
-  {
-    STAR_disposeTransferOperation(pTxTransferOp);
-  }
-
+  /* Dispose of the transfer operations */  
   if (pRxTransferOp != NULL)
   {
     STAR_disposeTransferOperation(pRxTransferOp);
   }
 
-  if (pFillPacket != NULL){
-    RMAP_FreeBuffer(pFillPacket);
-  }
   /* Close the channels */
   if (rxChannelId != 0U)
   {
     STAR_closeChannel(rxChannelId);
-  }
-
-  if (txChannelId != 0U)
-  {
-    STAR_closeChannel(txChannelId);
   }
 
   return 0;
